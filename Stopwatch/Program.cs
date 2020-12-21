c1de4075ffaf7ac0e0d05dbaa86b65ef1e246ead
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-12-21 16:08:37 +0200

    first commit

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatchcal = new StopwatchCalculations();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatchcal.Start();
+                Thread.Sleep(1000);
+
+                stopwatchcal.Stop();
+                Console.WriteLine($"Duration {stopwatchcal.GetInterval()}");
+
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
